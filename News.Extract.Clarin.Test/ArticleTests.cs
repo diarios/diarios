b4e794232b79cc31ec16303143643d55f5d2beef
Juan Manuel Lallana <juan.manuel.lallana@gmail.com>
2015-09-07 20:59:12 -0300

    Proyecto de prueba unitaria

---
+++ News.Extract.Clarin.Test/ArticleTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using NMock;
+using HtmlAgilityPack;
+using System.Xml;
+using News.Core;
+using System.IO;
+
+namespace News.Extract.Clarin.Test
+{
+	[TestFixture] public class ArticleTests
+	{
+		[Test] public void SimpleCaseTest ()
+		{
+			HtmlDocument doc = new HtmlDocument();
+			doc.LoadHtml("<h1>the title</h1>");
+			var buff = new StringWriter ();
+
+			IExtractor ext = new Article ();
+			ext.Accept (doc.DocumentNode, new XmlTextWriter(buff));
+
+			Assert.AreEqual ("<article title=\"the title\" />", buff.ToString());
+		}
+	}
+}
